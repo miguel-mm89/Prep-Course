f332e1f25c1285e796b412b70299a5954c41f3d3
miguelm89 <mikemorales89@live.com>
2022-07-10 06:43:49 -0300

    faltan solo 6 ejercicios

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -88,6 +88,11 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero > 20 && numero < 50){
+    return true
+  } else {
+    return false
+  }
 }
 
 function esEntero(numero) {
@@ -105,6 +110,17 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 3 === 0 && numero % 5 === 0){
+    return 'fizzbuzz'
+  }
+  else if (numero % 3 === 0){
+    return 'fizz'
+  } else if (numero % 5 === 0){
+    return 'buzz'
+  } 
+   else {
+    return numero
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -114,6 +130,9 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+
+
+
 }
 
 function esPrimo(numero) {
@@ -148,6 +167,13 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  let i = 0
+  do{
+    i++;
+    numero = numero + 5
+  } 
+  while(i < 8)
+  return numero
 }
 
 
