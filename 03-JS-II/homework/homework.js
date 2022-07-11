a872f96a18f59f5cd98271f8de754972f416ea95
miguelm89 <mikemorales89@live.com>
2022-07-11 06:28:08 -0300

    homework OK

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -103,6 +103,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  
 }
 
 function fizzBuzz(numero) {
@@ -125,13 +126,23 @@ function fizzBuzz(numero) {
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
-  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
+  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1   es mayor y positivo"
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-
-
+if(num1 ===0 || num2 === 0 || num3 === 0){
+return 'Error'
+} else if (num1 < 0 || num2 < 0 || num3 < 0) {
+return "Hay negativos"
+} else if (num1 > num2 && num1 > num3){
+  return "Número 1 es mayor y positivo"
+}
+else if(num3 > num1 && num3 > num2) {
+  return num3+1
+} else{
+  return false
+}
 
 }
 
@@ -141,25 +152,52 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if(numero == 2){
+    return true
+  } else if (numero < 2){
+    return false
+  } 
+  for(let i = 2; i < numero; i++){
+    if(numero % i === 0){
+      return false
+    }
+  }
+  return true
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if(valor == true){
+    return 'Soy verdadero'
+  } else if(valor == false) {
+    return 'Soy falso'
+  }
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  let Array = []
+  for(let i = 0; i <= 60; i++){
+    if(i % 6 == 0){
+     Array.push(i)
+    }
+  }
+  return Array
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  const number = numero.toString()
+  if(number.length == 3){
+    return true
+  } else {
+    return false
+  }
   
 }
 
